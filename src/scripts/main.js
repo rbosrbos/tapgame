dcd9f730ea5665d90eff47a09f78b3158ee1413c
Rui Silva <ruibatisilva@sapo.pt>
2020-01-29 21:46:43 +0000

    Almost final

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,14 @@
-const elGo = document.querySelector('#go');
-const elContainer = document.querySelector('#gameContainer');
-const elScored = document.querySelector('.scored');
-const elGoBack = document.querySelector('#goBack');
+let elGo = document.querySelector('#go');
+let elHeader = document.querySelector('header');
+let elContainer = document.querySelector('#gameContainer');
+let elScored = document.querySelector('.scored');
+let elGoBack = document.querySelector('#goBack');
 let elAllCells;
 let cellsHTML = '';
 let clicked = [];
-let timeout = [];
+let comparing = [];
+let timer = 0;
+let intTimer = 0;
 const icons = new Array(
   'glass',
   'music',
@@ -30,6 +33,13 @@ const icons = new Array(
 let gridArray = [];
 let usedIndexesArray = [];
 elGo.onclick = function () {
+  document.querySelector("select").style.display = 'none';
+  document.querySelector("button").style.display = 'none';
+  document.querySelector("header span").style.display = 'none';
+  intTimer = setInterval(function () {
+    timer = timer + 1000;
+    elHeader.innerHTML = "Tempo de jogo: " + (timer/1000) + "s";
+  }, 1000);
   const elLevel = document.querySelector('#level').options.selectedIndex;
   switch (elLevel) {
     default:
@@ -54,19 +64,36 @@ elGo.onclick = function () {
       if (clicked.length < 2) {
         elAllCells[i].style.display = 'none';
         if (clicked.includes(elAllCells[i].attributes.placeholder.value)) {
-          usedIndexesArray.splice(usedIndexesArray.indexOf(elAllCells[i].attributes.placeholder.value),1);
-          timeout.forEach(function (e) {
-            clearTimeout(e);
-          });
+          usedIndexesArray.splice(usedIndexesArray.indexOf(elAllCells[i].attributes.placeholder.value), 1);
           clicked = [];
+          comparing = [];
           checkIfDone();
         } else {
           clicked.push(elAllCells[i].attributes.placeholder.value);
-          timeout[i] = setTimeout(function () {
-            elAllCells[i].style.display = 'flex';
-            clicked = [];
-          }, 1000);
+          if (clicked.length == 2) {
+            comparing.push(elAllCells[i].attributes.placeholder.value);
+            setTimeout(function () {
+              elAllCells.forEach(function (e) {
+                if (comparing.includes(e.attributes.placeholder.value)) {
+                  e.style.display = 'flex';
+                }
+              });
+              comparing = [];
+              clicked = [];
+            }, 500);
+          } else {
+            comparing.push(elAllCells[i].attributes.placeholder.value);
+          }
         }
+      }
+      else {
+        elAllCells.forEach(function (e) {
+          if (comparing.includes(e.attributes.placeholder.value)) {
+            e.style.display = 'flex';
+          }
+        });
+        comparing = [];
+        clicked = [];
       }
     }
   }
@@ -98,13 +125,14 @@ function fillContainer(n) {
 }
 
 function checkIfDone() {
-if (usedIndexesArray.length == 0){
-  elScored.style.opacity = '1';
-  elScored.style.zIndex = '2';
+  if (usedIndexesArray.length == 0) {
+    elScored.style.opacity = '1';
+    elScored.style.zIndex = '2';
+    clearInterval(intTimer);
+    intTimer = 0;
+    document.querySelector(".time").innerHTML = (timer / 1000) + "s";
+  }
 }
-}
-elGoBack.onclick = function() {
-  elContainer.innerHTML = '';
-  elScored.style.opacity = '0';
-  elScored.style.zIndex = '-1';
+elGoBack.onclick = function () {
+  location.reload();
 }
